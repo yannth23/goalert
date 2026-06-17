2ebc5e3ed381a3876655893aae6a18816f31dc5c
yannth23 <45675559+yannth23@users.noreply.github.com>
2026-06-17 19:12:26 -0300

    feat: import WhatsappModule in JobsModule

--- backend/src/jobs/jobs.module.ts
+++ backend/src/jobs/jobs.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { EmailModule } from '../email/email.module';
 import { FootballMatchModule } from '../football-match/football-match.module';
+import { WhatsappModule } from '../whatsapp/whatsapp.module';
 import { DailyEmailService } from './daily-email.service';
 import { KeepAliveService } from './keep-alive.service';
 
 @Module({
-  imports: [PrismaModule, EmailModule, FootballMatchModule],
+  imports: [PrismaModule, EmailModule, FootballMatchModule, WhatsappModule],
   providers: [DailyEmailService, KeepAliveService],
 })
 export class JobsModule {}
