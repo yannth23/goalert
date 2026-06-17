603c1514d7772f17a68faa1494c748329690055b
yannth23 <45675559+yannth23@users.noreply.github.com>
2026-06-17 19:22:46 -0300

    fix: import WhatsappModule in FootballMatchModule

--- backend/src/football-match/football-match.module.ts
+++ backend/src/football-match/football-match.module.ts
@@ -4,9 +4,10 @@ import { FootballMatchController } from './football-match.controller';
 import { FootballApiService } from './football-api.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { RedisModule } from '../redis/redis.module';
+import { WhatsappModule } from '../whatsapp/whatsapp.module';
 
 @Module({
-  imports: [PrismaModule, RedisModule],
+  imports: [PrismaModule, RedisModule, WhatsappModule],
   controllers: [FootballMatchController],
   providers: [FootballMatchService, FootballApiService],
   exports: [FootballMatchService, FootballApiService],
