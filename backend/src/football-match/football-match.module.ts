5003c0467bf5e56059854be15f936156b6a38048
yannth23 <45675559+yannth23@users.noreply.github.com>
2026-06-18 01:28:25 -0300

    refactor: import TelegramModule in FootballMatchModule

--- backend/src/football-match/football-match.module.ts
+++ backend/src/football-match/football-match.module.ts
@@ -4,11 +4,10 @@ import { FootballMatchController } from './football-match.controller';
 import { FootballApiService } from './football-api.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { RedisModule } from '../redis/redis.module';
-import { WhatsappModule } from '../whatsapp/whatsapp.module';
 import { TelegramModule } from '../telegram/telegram.module';
 
 @Module({
-  imports: [PrismaModule, RedisModule, WhatsappModule, TelegramModule],
+  imports: [PrismaModule, RedisModule, TelegramModule],
   controllers: [FootballMatchController],
   providers: [FootballMatchService, FootballApiService],
   exports: [FootballMatchService, FootballApiService],
