e41dd4f3828300cfac849ab7386b09f11c9a46f0
yannth23 <45675559+yannth23@users.noreply.github.com>
2026-07-12 00:20:02 -0300

    feat: register BracketSeedService and BracketService in FootballMatchModule

--- backend/src/football-match/football-match.module.ts
+++ backend/src/football-match/football-match.module.ts
@@ -5,13 +5,30 @@ import { FootballApiService } from './football-api.service';
 import { ScraperService } from './scraper.service';
 import { StatisticsPredictorService } from './statistics-predictor.service';
 import { TeamReportService } from './team-report.service';
+import { BracketSeedService } from './bracket-seed.service';
+import { BracketService } from './bracket.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { RedisModule } from '../redis/redis.module';
 
 @Module({
   imports: [PrismaModule, RedisModule],
   controllers: [FootballMatchController],
-  providers: [FootballMatchService, FootballApiService, ScraperService, StatisticsPredictorService, TeamReportService],
-  exports: [FootballMatchService, FootballApiService, ScraperService, StatisticsPredictorService, TeamReportService],
+  providers: [
+    FootballMatchService,
+    FootballApiService,
+    ScraperService,
+    StatisticsPredictorService,
+    TeamReportService,
+    BracketSeedService,
+    BracketService,
+  ],
+  exports: [
+    FootballMatchService,
+    FootballApiService,
+    ScraperService,
+    StatisticsPredictorService,
+    TeamReportService,
+    BracketService,
+  ],
 })
 export class FootballMatchModule {}
